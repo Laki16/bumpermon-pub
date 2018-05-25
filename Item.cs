88459489e8a8ee9273d3ec6fa40b958d76c03435
Laki16 <lkanen16@gmail.com>
2018-05-25 12:38:53 +0900

    no message

--- Item.cs
+++ Item.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Item : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum ItemType
+    {
+        MINE,
+        LIFE,
+        COIN,
+        NITRO
+    }
+
+    
 }
