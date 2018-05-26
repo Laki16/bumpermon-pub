af0f906cba6dc9300fc97431c331da4774d4597e
Cradirow <hanorang2@naver.com>
2018-05-26 17:34:42 +0900

    add concept scene

---
+++ StarColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarColor : MonoBehaviour {
+
+    public float timeInterval = 2.0f;
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ColorChangeRoutine());
+	}
+
+    private IEnumerator ColorChangeRoutine()
+    {
+        float timer = 0.0f;
+        Color32 color = new Color32((byte)Random.Range(0, 255), (byte)Random.Range(0, 255), (byte)Random.Range(0, 255), 255);
+
+        while (timer < timeInterval)
+        {
+            timer += Time.deltaTime * speed;
+            GetComponent<Renderer>().material.color = Color32.Lerp(GetComponent<Renderer>().material.color, color, timer);
+            yield return null;
+        }
+
+        // On finish recursively call the same routine
+        StartCoroutine(ColorChangeRoutine());
+    }
+}
