71af6f33b8d019105a6df0d2144e33c119ab0c20
Cradirow <hanorang2@naver.com>
2018-05-27 14:16:41 +0900

    revert cutscene

---
+++ CutEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutEvent : MonoBehaviour {
+
+    public GameObject golemDummy;
+
+    public void Pop(){
+        golemDummy.transform.position += new Vector3(0, 0.5f, 0);
+        golemDummy.GetComponent<Animator>().SetTrigger("Pop");
+    }
+}
