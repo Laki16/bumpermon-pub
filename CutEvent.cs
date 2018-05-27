df1d13eb7bbfc2e22fbb4dd73bd126ce454dc19f
Cradirow <hanorang2@naver.com>
2018-05-27 20:51:47 +0900

    Complete full timeline.
    Before start the timeline, be sure 'GolemPlayer' unenabled and 'Polygonal Golem Orange' is enabled in Boxes>GolemBox.
    Also, 'Polygonal Golem Orange' y value should be 0.8f .
    ** P.S. Tutorial FX and sound unworking.

--- CutEvent.cs
+++ CutEvent.cs
@@ -6,23 +6,37 @@ public class CutEvent : MonoBehaviour {
 
     public GameObject golemDummy;
     public GameObject disapearBlock;
+    public GameObject leftArm;
+    public GameObject rightArm;
     bool isRotate = false;
+    public GameObject golemPlayer;
 
-	private void Update()
-	{
-        if(isRotate){
-            golemDummy.transform.Rotate(new Vector3(0, -1, 0) * 1.5f);
-            golemDummy.transform.position += new Vector3(.1f, 0, 0);
+    Vector3 targetPos = new Vector3(-8, 0, 0);
+    Quaternion targetRot = new Quaternion(0, 0, 0, 0);
+
+    private void Update()
+    {
+        if (isRotate)
+        {
+            golemDummy.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0) * Time.deltaTime);
+            golemDummy.transform.position = Vector3.Lerp(golemDummy.transform.position, targetPos, Time.deltaTime);
         }
-	}
+    }
 
-	public void Pop(){
+    public void Pop(){
         golemDummy.transform.position += new Vector3(0, 0.5f, 0);
         golemDummy.GetComponent<Animator>().SetTrigger("Pop");
+
+        leftArm.GetComponent<Animator>().SetTrigger("Pop");
     }
 
     public void Guard(){
         golemDummy.GetComponent<Animator>().SetTrigger("Guard");
+    }
+
+    public void Punch()
+    {
+        golemDummy.GetComponent<Animator>().SetTrigger("Punch");
     }
 
     public void Jump()
@@ -38,5 +52,16 @@ public class CutEvent : MonoBehaviour {
 
     public void DeleteBlock(){
         disapearBlock.SetActive(false);
+    }
+
+    public void Angry()
+    {
+        rightArm.GetComponent<Animator>().SetTrigger("Angry");
+    }
+
+    public void TutorialStart()
+    {
+        golemDummy.SetActive(false);
+        golemPlayer.SetActive(true);
     }
 }
