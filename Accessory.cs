8f0d0426df59ee1a333938a9f5df5afe21559602
Laki16 <lkanen16@gmail.com>
2018-07-11 02:55:07 +0900

    no message

---
+++ Accessory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accessory : ShopManager {
+
+    [Header("Accessory Info")]
+    public int itemNumber;
+    protected bool isUnlock = false;
+    protected int enhance;
+    protected int maxEnhance = 9;
+
+    [Header("State Info")]
+    public bool isEquip;
+}
--- ShopManager.cs
+++ ShopManager.cs
@@ -3,6 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShopManager : MonoBehaviour {
+    //Accessary Index
+    //  0001 : 
+    //private static int maxAccessorys = 100;
+    public List<Accessory> accessories = new List<Accessory>();
+    
+    [Header("Accessory Effect")]
+    public int HP;
+    public int SPD;
+    public int DEF;
+    public int STR;
+    public int LUK;
+    [Space(10)]
+    public float nitroRange;
+    public float nitroTime;
+
+    [Header("Equipment")]
+    private int maxEquipment = 3;
+    public List<Accessory> equipment = new List<Accessory>();
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +31,12 @@ public class ShopManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void ApplyAccessory()
+    {
+        for(int i = 0; i< equipment.Count; i++)
+        {
+            //스탯 적용
+        }
+    }
 }
