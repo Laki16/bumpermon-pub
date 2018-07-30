85b11824b686395dbc0cdf81522ddff1aedc6f8c
Cradirow <hanorang2@naver.com>
2018-07-30 17:12:08 +0900

    Add UI materials

---
+++ UnityAdsHelper.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+ 
+public class UnityAdsHelper : MonoBehaviour
+{
+    private const string android_game_id = "2703328";
+    private const string ios_game_id = "xxxxxxx";
+
+    private const string video_id = "video";
+    private const string rewarded_video_id = "rewardedVideo";
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+#if UNITY_ANDROID
+        Advertisement.Initialize(android_game_id);
+#elif UNITY_IOS
+        Advertisement.Initialize(ios_game_id);
+#endif
+    }
+
+    public void ShowAd()
+    {
+        if (Advertisement.IsReady(video_id))
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+
+            Advertisement.Show(video_id, options);
+        }
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady(rewarded_video_id))
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+
+            Advertisement.Show(rewarded_video_id, options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                {
+                    Debug.Log("The ad was successfully shown.");
+
+                    Time.timeScale = 1;
+
+                    break;
+                }
+            case ShowResult.Skipped:
+                {
+                    Debug.Log("The ad was skipped before reaching the end.");
+
+                    Time.timeScale = 1;
+
+                    break;
+                }
+            case ShowResult.Failed:
+                {
+                    Debug.LogError("The ad failed to be shown.");
+
+                    // to do ...
+                    // 광고 시청에 실패했을 때 처리
+
+                    break;
+                }
+        }
+    }
+}
